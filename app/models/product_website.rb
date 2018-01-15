12c75670f1f824ae4373d85afb360b3fa433c985
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2018-01-15 18:05:29 +0000

    Change ProductsController, add lots to InitialWebScrape
    Close to adding dynamic website add function. If the website doesnt
    exist but the product has all the attributes(Meta tags) then the website will be
    added aswell as the product. If website present, standard procedure and
    everythings added normally

--- app/models/product_website.rb
+++ app/models/product_website.rb
@@ -1,5 +1,12 @@
 class ProductWebsite < ApplicationRecord
   has_many :products
-
   validates :website_url, :product_price_name, :now_price_name, presence: true
+
+  def self.add_website(website_url:, product_price_name:, now_price_name:)
+    ProductWebsite.create(
+      website_url: website_url,
+      product_price_name: product_price_name,
+      now_price_name: now_price_name
+    )
+  end
 end
--- app/services/initial_web_scrape.rb
+++ app/services/initial_web_scrape.rb
@@ -1,11 +1,14 @@
+require 'open-uri'
+
 class InitialWebScrape
-  attr_accessor :page, :product, :user_agent, :attributes
+  attr_accessor :page, :product, :user_agent, :attributes, :new_website
 
   def initialize(product:)
     @product = product
     @user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
     @page = Nokogiri::XML(open(product.product_url,'User-Agent' => user_agent), nil, "UTF-8")
     @attributes = {}
+    @new_website = {}
   end
 
   def self.call(*args)
@@ -13,13 +16,18 @@ class InitialWebScrape
   end
 
   def call
-    get_info
+    if product.product_website == nil
+      has_everything?
+    end
+    get info
+
     Product.save_product_attributes(
                                 product: product,
                                 price: attributes[:price],
                                 name: attributes[:title],
                                 image_url: attributes[:image],
-                               )
+                                product_website_id: attributes[:product_website_id]
+    )
   end
 
   def correct_price_format(price)
@@ -27,18 +35,54 @@ class InitialWebScrape
   end
 
   def get_info
-    attributes[:title] = page.at('meta[property="og:title"]')['content']
-    attributes[:image] = page.at('meta[property="og:image"]')['content']
-    get_price
+    page.remove_namespaces!
+    #add title and image column to ProductWebsite then alter 2 lines below
+    attributes[:title] = page.xpath('meta[property="og:title"]')['content']
+    attributes[:image] = page.xpath('meta[property="og:image"]')['content']
+    attributes[:price] = page.xpath(product.product_website.product_price_name).text
+    attributes[:product_website_id] = product.product_website.id
+    correct_price_format(attributes[:price])
   end
 
-  def get_price
+
+
+#METAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+  def has_everything?
     page.remove_namespaces!
-    if page.xpath('//meta[contains(@property,"price:amount")]') != nil
-      attributes[:price] = page.xpath('//meta[contains(@property,"price:amount")]/@content').text
-    else product.product_website.product_price_name != nil
-      attributes[:price] = page.xpath(product.product_website.product_price_name).text
+    attributes[:price] = page.xpath('//meta[contains(@property,"price:amount")]/@content').text
+    attributes[:title] = page.xpath('//meta[contains(@property,"title")]/@content').text
+    attributes[:image] = page.xpath('//meta[contains(@property,"image")]/@content').text
+    if attributes[:price] != nil &&
+      attributes[:title] != nil &&
+      attributes[:image] != nil
+      get_website_info
     end
-    correct_price_format(attributes[:price])
+  end
+
+  def save_website
+    ProductWebsite.add_website(
+      website_url: new_webiste[:website_url],
+      product_price_name: new_website[:product_price_name],
+      now_price_name: new_website[:now_price_name]
+    )
+  end
+
+  def get_website_info
+    new_website[:website_url] = url_host(product.product_website)
+    new_website[:product_price_name] = '//meta[contains(@property,"price:amount")]/@content'
+    new_website[:now_price_name] = '//meta[contains(@property,"price:amount")]/@content'
+    #add image and title here website
+    save_website
+  end
+
+  def url_host(url)
+    URI(url).host
+  end
+
+  def product_website
+    ProductWebsite.all.select do |saved_website|
+      params[:product].values.first.include?(saved_website.website_url)
+    end.first
   end
 end
