7cad02a7f692261a8613a15eaaf3ee183607d238
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2018-01-18 15:43:02 +0000

    Like the branch says, works but is bad practice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,54 @@
 class ProductsController < ApplicationController
   include ApplicationHelper
 
+  before_action :page_scrape, only: [:create]
+
   def new
     @product = Product.new
   end
 
   def create
-    user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
-    @page = Nokogiri::XML(open(product_url,'User-Agent' => user_agent), nil,"UTF-8")
-    if return_website != nil
-      call_initial_scrape
-    else
-      MetaWebsiteAdd.call(product_url: product_url, page: @page)
-      call_initial_scrape
+    if return_website.blank?
+      MetaWebsiteAdd.call(product_url: product_url, page: page_scrape)
     end
-    flash[:success] = "'#{current_user.products.last.name}' has been saved"
+    if return_website.present?
+      call_initial_scrape
+      product_saved?
+    else
+      flash[:error] = "Website not supported at this time!"
+      redirect_to new_product_path
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
     redirect_to "/users/#{current_user.id}"
   end
 
- private
+private
+
+  def product_params
+    params.require(:product).permit(:product_url)
+  end
+
+  def page_scrape
+    user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
+    Nokogiri::XML(open(product_url,'User-Agent' => user_agent), nil,"UTF-8")
+  end
+
+  def product_saved?
+    if current_user.products.last.product_url == product_url
+      flash[:success] = "'#{current_user.products.last.name}' has been saved"
+      redirect_to "/users/#{current_user.id}"
+    else
+      flash[:error] = "Product could not be saved, try a different item!"
+      redirect_to new_product_path
+    end
+  end
 
   def call_initial_scrape
-    InitialWebScrape.call(product_url: product_url, page: @page, user: current_user)
+    InitialWebScrape.call(product_url: product_url, page: page_scrape, user: current_user)
   end
 
   def product_url
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-
   root 'home#index'
-  get '/product', to: 'products#new'
-  post '/product', to: 'products#create'
-
-  get '/productwebsites/new', to: 'product_websites#new'
-  post '/productwebsites/new', to: 'product_websites#create'
+  resources :users
+  resources :products, only: [:new, :create, :destroy]
+  resources :product_websites, only: [:new, :create]
+  resources :web_scrapes, only: :create
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-
-  resource :web_scrapes, only: :create
 
 end
