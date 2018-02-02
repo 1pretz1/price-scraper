0ee8af7ecc8846461d2b4b7a0c4f13acd2b9c954
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2018-02-02 15:03:36 +0000

    Will now scrape a js rendered price if there is an existing ProductWebsite
    Uses selenium web driver using chrome headless browser to scrape instead
    of Nokogiri (as only parses html). Title and image still acquired with
    meta tags however using the browser. For the price an xpath specific to
    the website is used. This version works, however more effort is needed
    to put in fail safes and make the process more effient.

--- app/services/ajax_scrape.rb
+++ app/services/ajax_scrape.rb
@@ -1,12 +1,16 @@
 require 'selenium-webdriver'
 
-class AjaxScrape
-  attr_accessor :driver
+class AjaxScrape < InitialWebScrape
+  attr_accessor :driver, :website, :user, :attributes, :url
 
-  def initialize(url:, website:)
+  def initialize(url:, user:, website:)
     options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
-    driver = Selenium::WebDriver.for(:chrome, options: options)
-    driver.get(url)
+    @driver = Selenium::WebDriver.for(:chrome, options: options)
+    @url = url
+    @driver.get(url)
+    @website = website
+    @user = user
+    @attributes = {}
   end
 
   def self.call(*args)
@@ -19,8 +23,24 @@ class AjaxScrape
 
   def get_info
     price = driver.find_element(xpath: website.price_xpath).text
-    image = driver.find_element(xpath: website.image_xpath).text
-    title = driver.find_element(xpath: website.title_xpath).text
+    attributes[:price] = correct_price_format(price)
+
+    #META tags - attribute('content')
+
+    attributes[:image] = driver.find_element(xpath: website.image_xpath)
+                         .attribute('content')
+    attributes[:title] = driver.find_element(xpath: website.title_xpath)
+                         .attribute('content')
     driver.quit
+  end
+
+  def save_info
+    user.products.create(
+      product_url: url,
+      price: attributes[:price],
+      name: attributes[:title],
+      image_url: attributes[:image],
+      product_website_id: website.id
+    )
   end
 end
