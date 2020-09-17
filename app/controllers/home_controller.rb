2bbbef21bb8667090aa0f72084dc6071f2799d8f
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2020-09-17 11:57:05 +0100

    [GIT TEST]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
-
   def index
-    @products = Product.order("RANDOM()").limit(9)
+    @products = Product.order("RANDOM()").limit(8)
   end
 
   def show; end
